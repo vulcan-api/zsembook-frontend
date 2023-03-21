fabe94d058db1b889d77d2780bb3f819c970fc16
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-03-21 20:52:19 +0100

    Remove permission to settings when user is not logged

--- src/Layout/Sidebar.tsx
+++ src/Layout/Sidebar.tsx
@@ -162,6 +162,10 @@ const Sidebar = () => {
                 destination: "/settings",
                 label: "Ustawienia",
                 icon: <Icon.Tools />,
+                  className: Object.keys(user).length === 0 ? "tooltip" : "",
+                tooltipText: "Zaloguj się, aby mieć dostęp",
+                tooltipBtm: "50%",
+                isBlocked: true,
                 onClick: () => {
                   setIsShown(false);
                 },
