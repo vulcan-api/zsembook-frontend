095dd090db855de2b8136556ec59092ecf47c6ee
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-03-21 21:03:23 +0100

    Fix login bug

--- src/Sites/auth/Login/Login.tsx
+++ src/Sites/auth/Login/Login.tsx
@@ -58,50 +58,57 @@ const Login = () => {
             password: passwordRef.current.value,
         });
 
-        await fetch("http://localhost:3000/auth/login", {
+        const response = await fetch("http://localhost:3000/auth/login", {
             method: "POST",
             headers: myHeaders,
             body: raw,
             redirect: "follow",
             credentials: "include",
         })
-            .then((response) => response.text())
-            .then(() => {
-                navigate("/");
-            })
+            .then((res) => res.json())
             .catch((error) => {
                 console.log("error", error)
                 NotificationManager.error(
-                  "Nie udało się zalogować. Spróbuj ponownie później",
-                  "Nie zalogowano",
-                  3000
+                    "Nie udało się zalogować. Spróbuj ponownie później",
+                    "Nie zalogowano",
+                    3000
                 );
             });
+        console.log(response)
+        if (response.token) {
+            navigate("/");
+        } else {
+            NotificationManager.error(
+                "Podałeś błędne dane",
+                "Wystąpił błąd",
+                3000
+            );
+        }
     };
 
     return (
-      <div className={classes.loginFlex}>
-        <div className={classes.img}></div>
-        <div className={classes.formSide}>
-          <div className={classes.loginForm}>
-            <p>Zaloguj się</p>
-            <img src={loginImg} alt="cool login img" />
-            <form onSubmit={loginHandler} className={classes.form}>
-              <Input placeholder="E-Mail" ref={emailRef} />
-              <Input type="password" placeholder="Hasło" ref={passwordRef} />
-              <p>Nie pamiętasz hasła?</p>
-              <Checkbox
-                ref={remeberPasswordRef}
-                id="passwordRemember"
-                label="Zapamiętaj hasło"
-              />
-              <Button type="submit" buttonText="Zaloguj się" />
-            </form>
-            <Link to={"/auth/signup"}>Nie masz konta? Zarejestruj się!</Link>
-            <p onClick={() => navigate("/")}>Kontynuuj bez logowania</p>
-          </div>
+        <div className={classes.loginFlex}>
+            <div className={classes.img}></div>
+            <div className={classes.formSide}>
+                <div className={classes.loginForm}>
+                    <p>Zaloguj się</p>
+                    <img src={loginImg} alt="cool login img"/>
+                    <form onSubmit={loginHandler} className={classes.form}>
+                        <Input placeholder="E-Mail" ref={emailRef}/>
+                        <Input type="password" placeholder="Hasło" ref={passwordRef}/>
+                        <p>Nie pamiętasz hasła?</p>
+                        <Checkbox
+                            ref={remeberPasswordRef}
+                            id="passwordRemember"
+                            label="Zapamiętaj hasło"
+                        />
+                        <Button type="submit" buttonText="Zaloguj się"/>
+                    </form>
+                    <Link to={"/auth/signup"}>Nie masz konta? Zarejestruj się!</Link>
+                    <p onClick={() => navigate("/")}>Kontynuuj bez logowania</p>
+                </div>
+            </div>
         </div>
-      </div>
     );
 };
 
