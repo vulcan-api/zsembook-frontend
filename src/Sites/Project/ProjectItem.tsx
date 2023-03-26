0b0ed0f07946e4297c53f1924e5c9480bbc3a45d
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-03-26 19:28:50 +0200

    Link to facebook in new tab

--- src/Sites/Project/ProjectItem.tsx
+++ src/Sites/Project/ProjectItem.tsx
@@ -5,38 +5,49 @@ import React from "react";
 import Button from "../../Components/Button";
 
 const ProjectItem = (props: any) => {
-  const project = props.project;
-  return (
-    <div style={props.listType}>
-      <Wrapper className={classes.post}>
-        <div className={classes.topData}>
-          <div>
-            <Icon.CalendarDate />
-            {new Date(project.date).toLocaleDateString()}
-          </div>
+    const project = props.project;
+    const isMobile = /Mobile/.test(navigator.userAgent);
+    const handleFacebookClick = () => {
+        if (isMobile) {
+            window.location.replace(project.facebook);
+        } else {
+            window.open(project.facebook, '_blank');
+        }
+    }
+    const handleWebsiteClick = () => {
+        if (isMobile) {
+            window.location.replace(project.website);
+        } else {
+            window.open(project.website, '_blank');
+        }
+    }
+    return (
+        <div style={props.listType}>
+            <Wrapper className={classes.post}>
+                <div className={classes.topData}>
+                    <div>
+                        <Icon.CalendarDate/>
+                        {new Date(project.date).toLocaleDateString()}
+                    </div>
+                </div>
+                <h2>{project.title}</h2>
+                <p className={classes.content}>{project.text}</p>
+                <div className={classes.buttonFlex}>
+                    <Button
+                        buttonText="Facebook"
+                        icon={<Icon.Facebook/>}
+                        onClick={handleFacebookClick}
+                    />
+                    <Button
+                        buttonText="Strona szkoły"
+                        icon={<Icon.BrowserFirefox/>}
+                        className="alternate"
+                        onClick={handleWebsiteClick}
+                    />
+                </div>
+            </Wrapper>
         </div>
-        <h2>{project.title}</h2>
-        <p className={classes.content}>{project.text}</p>
-        <div className={classes.buttonFlex}>
-          <Button
-            buttonText="Facebook"
-            icon={<Icon.Facebook />}
-            onClick={() => {
-              window.location.replace(project.facebook);
-            }}
-          />
-          <Button
-            buttonText="Strona szkoły"
-            icon={<Icon.BrowserFirefox />}
-            className="alternate"
-            onClick={() => {
-              window.location.replace(project.website);
-            }}
-          />
-        </div>
-      </Wrapper>
-    </div>
-  );
+    );
 };
 
 export default ProjectItem;
