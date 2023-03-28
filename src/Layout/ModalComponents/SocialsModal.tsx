4deaece75810dbc775d5c147fe23b315edd5ac53
cooligus <pewienjaaa@gmail.com>
2023-03-28 18:49:05 +0200

    Add rest of files

--- src/Layout/ModalComponents/SocialsModal.tsx
+++ src/Layout/ModalComponents/SocialsModal.tsx
@@ -1,22 +1,61 @@
-import React, { useEffect } from "react";
+import React, { ReactNode, useCallback, useEffect, useState } from "react";
+import { BrowserFirefox, Facebook, Instagram, Youtube } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
+import classes from "./SocialsModal.module.css";
 
-const SocialsModal = (props: {
+interface SocialsModalProps {
   userId: Number,
-  onClose: Function;
-  showSpinner: Function
-}) => {
+  onClose: Function,
+  showSpinner: Function,
+}
+
+interface SocialElementProps {
+  icon: ReactNode, 
+  name: string, 
+  url: string,
+  fullUrl?: boolean,
+}
+
+interface SocialsType {
+  facebook: string,
+  instagram: string,
+  youtube: string,
+  website: string,
+}
+
+const SocialElement = (props: SocialElementProps) => 
+  <Link to={props.url + (props.fullUrl ? '' : props.name)} className={classes.socialElem} target='_blank'>
+    <span className={classes.icon}>
+      {props.icon}
+    </span>
+    <span className={classes.name}>
+      {props.name}
+    </span>
+  </Link>
+
+const SocialsModal = (props: SocialsModalProps) => {
+  const [socials, setSocials] = useState<SocialsType>();
+
+  const getSocials = useCallback(async () => {
+      fetch(`${process.env.REACT_APP_REQUEST_URL}/user/${props.userId}`)
+      .then(res => res.json())
+      .then(json => setSocials(json))
+  }, [props.userId]);
 
   useEffect(() => {
     props.showSpinner(false);
-  }, [props]);
+    getSocials();
+  }, [props, getSocials]);
 
-  return (
-    <>
-      <p>Obserwujący: </p>
-      <ul>
-      </ul>
-    </>
-  );
+  return <>
+    <p>Social media</p>
+    <ul>
+      {socials?.facebook && <SocialElement icon={<Facebook/>} name={socials.facebook} url='https://facebook.com/' />}
+      {socials?.instagram && <SocialElement icon={<Instagram/>} name={socials.instagram} url='https://instagram.com/' />}
+      {socials?.youtube && <SocialElement icon={<Youtube/>} name={socials.youtube} url='https://youtube.com/@' />}
+      {socials?.website && <SocialElement icon={<BrowserFirefox/>} name={new URL(socials.website).hostname} url={socials.website} fullUrl={true}/>}
+    </ul>
+  </>
 };
 
 export default SocialsModal;
