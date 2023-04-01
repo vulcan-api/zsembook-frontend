2fc4a155967149c925b6f7557ce29fa62724b274
Tomasz Mamala <tomaszmamala@gmail.com>
2023-04-01 17:18:19 +0200

    fixed css bug which was shrinking radio

--- src/Components/Radio.tsx
+++ src/Components/Radio.tsx
@@ -1,31 +1,35 @@
 import classes from "./Radio.module.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Radio = (props: any) => {
-    const [selectedValue, setSelectedValue] = useState("");
+  const [selectedValue, setSelectedValue] = useState("");
 
-    const handleSelectionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSelectedValue(event.target.value);
-    };
-    return <span className={classes.main}>
-     {props.values.map((value: any, index: number) => {
-         const score = value.score;
-         return (
-             <span key={index}>
+  const handleSelectionChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSelectedValue(event.target.value);
+  };
+  return (
+    <div className={classes.main}>
+      {props.values.map((value: any, index: number) => {
+        const score = value.score;
+        return (
+          <div key={index}>
             <input
-                type="radio"
-                id={`${props.name}${index}`}
-                name={props.name}
-                value={JSON.stringify(score)}
-                className="radio"
-                checked={selectedValue === JSON.stringify(score)}
-                onChange={handleSelectionChange}
+              type="radio"
+              id={`${props.name}${index}`}
+              name={props.name}
+              value={JSON.stringify(score)}
+              className="radio"
+              checked={selectedValue === JSON.stringify(score)}
+              onChange={handleSelectionChange}
             />
             <label htmlFor={`${props.name}${index}`}>{value.label}</label>
-          </span>
-         );
-     })}
-  </span>;
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Radio;
