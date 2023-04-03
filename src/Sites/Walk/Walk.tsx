41e2b37e8735c2816c63cf0199b1b7e25e711d01
cooligus <pewienjaaa@gmail.com>
2023-04-03 12:02:58 +0200

    Add spinner to school component

--- src/Sites/Walk/Walk.tsx
+++ src/Sites/Walk/Walk.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import LoadingSpinner from "../../Components/LoadingSpinner";
 import classes from "./Walk.module.css";
 
 const Walk = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
     return (
         <>
         <div className={classes.walkCont}>
-            <iframe className={classes.iframe} title="SchoolInside" src="https://share.spotbrowser.com/f7d73886-299c-4063-aeaa-689abe3eed45-8433?lang=pl&amp;nc" allowFullScreen={true} frameBorder={0}></iframe>
+            {isLoading && <div className={classes.spinner}><LoadingSpinner /></div>}
+            <iframe className={classes.iframe} title="SchoolInside" src="https://share.spotbrowser.com/f7d73886-299c-4063-aeaa-689abe3eed45-8433?lang=pl&amp;nc" allowFullScreen={true} frameBorder={0} onLoad={() => setIsLoading(false)}></iframe>
         </div>
         </>
     )
